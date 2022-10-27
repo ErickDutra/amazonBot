32f21853deb350c42fd02897553e3cb2582886a1
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2022-10-27 01:31:17 -0300

    amazonbot
    seleciona os produtos e monitora

---
+++ amazon.py
@@ -0,0 +1,26 @@
+import datetime
+import csv
+from requests_html import HTMLSession
+
+s = HTMLSession()
+asins = []
+
+#leitura da lista de asins no csv 
+with open('asins.csv', 'r') as f:
+    csv_reader = csv.reader(f)
+    for row in csv_reader:
+        asins.append(row[0])
+
+#busca do preço e titulo 
+for asin in asins:
+    r = s.get(f'https://www.amazon.com.br/dp/{asin}')
+    r.html.render(sleep=1)
+    try:
+        price = r.html.find('#corePrice_feature_div')[0].text.replace('R$', ' #').replace(',', '.').strip()
+    except:
+        price = r.html.find('#apex_offerDisplay_desktop')[0].text.replace('R$', ' #').replace(',', '.').strip()
+    title = r.html.find('#productTitle')[0].text.strip()
+    asin = asin
+    date = datetime.datetime.today()
+    
+    print(title, price, date)
